44cea31ff27ea099ac3d03efaa4085f0343fd756
Thomas Kielbus <thomas@liveramp.com>
2014-01-20 16:22:37 -0800

    Minor cleanup

--- src/main/java/com/liveramp/megadesk/worker/NaiveWorker.java
+++ src/main/java/com/liveramp/megadesk/worker/NaiveWorker.java
@@ -24,6 +24,7 @@ import org.apache.log4j.Logger;
 
 import com.liveramp.megadesk.gear.BaseGearExecutor;
 import com.liveramp.megadesk.gear.Gear;
+import com.liveramp.megadesk.gear.GearExecutor;
 import com.liveramp.megadesk.gear.Outcome;
 
 public class NaiveWorker extends BaseWorker implements Worker {
@@ -61,7 +62,7 @@ public class NaiveWorker extends BaseWorker implements Worker {
 
     private static final long SLEEP_PERIOD_MS = 100;
 
-    private final BaseGearExecutor gearExecutor = new BaseGearExecutor();
+    private final GearExecutor gearExecutor = new BaseGearExecutor();
 
     @Override
     public void run() {
