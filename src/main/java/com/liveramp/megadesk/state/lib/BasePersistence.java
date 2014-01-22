d78c29e48aacabeb732e9f9ff258acd151437917
Porter Westling <pwestling@liveramp.com>
2014-01-22 10:25:15 -0800

    method is gone

--- src/main/java/com/liveramp/megadesk/state/lib/BasePersistence.java
+++ src/main/java/com/liveramp/megadesk/state/lib/BasePersistence.java
@@ -3,8 +3,4 @@ package com.liveramp.megadesk.state.lib;
 import com.liveramp.megadesk.state.Persistence;
 
 public abstract class BasePersistence<VALUE> implements Persistence<VALUE> {
-  @Override
-  public VALUE get() {
-    return read().get();
-  }
 }
