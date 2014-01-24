3429cd7030ade0132ade3b2456f2d955c38cf1de
Porter Westling <pwestling@liveramp.com>
2014-01-23 19:31:38 -0800

    add licenses

--- megadesk-recipes/src/main/java/com/liveramp/megadesk/recipes/queue/Erase.java
+++ megadesk-recipes/src/main/java/com/liveramp/megadesk/recipes/queue/Erase.java
@@ -1,3 +1,18 @@
+/**
+ *  Copyright 2014 LiveRamp
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.liveramp.megadesk.recipes.queue;
 
 import com.google.common.collect.ImmutableList;
@@ -6,6 +21,7 @@ import com.liveramp.megadesk.core.state.Driver;
 import com.liveramp.megadesk.core.state.Reference;
 import com.liveramp.megadesk.core.transaction.Context;
 import com.liveramp.megadesk.core.transaction.Dependency;
+
 import com.liveramp.megadesk.core.transaction.Transaction;
 
 class Erase implements Transaction<Void> {
